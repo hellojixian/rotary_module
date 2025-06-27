46eb088f89610c4277b367a99e6e9edfb9375f51
Jixian Wang <hellojixian@gmail.com>
2025-06-27 10:59:50 +0300

    更新了使用外部晶振的逻辑

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,32 @@ void setup() {
   // 初始化串口调试（可选）
   Serial.begin(115200);
 
+#if ((F_CPU) == 1000000)
+  #warning F_CPU = 1MHz
+  Serial.println("1MHz");
+  #elif ((F_CPU) == 2000000)
+  #warning F_CPU = 2MHz
+  Serial.println("2MHz");
+  #elif ((F_CPU) == 4000000)
+  #warning F_CPU = 4MHz
+  Serial.println("4MHz");
+  #elif ((F_CPU) == 8000000)
+  #warning F_CPU = 8MHz
+  Serial.println("8MHz");
+  #elif ((F_CPU) == 16000000)
+  #warning F_CPU = 16MHz
+  Serial.println("16MHz");
+  #elif ((F_CPU) == 32000000)
+  #warning F_CPU = 32MHz
+  Serial.println("32MHz");
+  #endif
+
+  #if ((CLOCK_SOURCE) == 1)
+  Serial.println("internal clock");
+  #elif ((CLOCK_SOURCE) == 2)
+  Serial.println("external clock");
+  #endif
+
   // 初始化 I2C 总线（默认使用 Wire，PC4 = SDA, PC5 = SCL）
   Wire.begin();
 
